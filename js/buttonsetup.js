89bed6daa24f7f0f79817aab6e5699640b55d1c6
Siegert-S <149582140+Siegert-S@users.noreply.github.com>
2024-07-22 00:55:50 +0200

    Handy 2

---
+++ js/buttonsetup.js
@@ -0,0 +1,9 @@
+function initButton() {
+    MenuButton.produce(10,10,100,25,'mainMenu','Play',()=>{console.log('Play is pressed');} );
+    MenuButton.produce(10,55,100,25,'mainMenu','Settings',()=>{console.log('Settings is pressed');});
+    MenuButton.produce(10,100,100,25,'mainMenu','Controlls',()=>{console.log('Controlls is pressed');});
+
+    MenuButton.produce(10,430,40,40,'controlls','left', ()=>{console.log('left');});
+    MenuButton.produce(60,430,40,40,'controlls','reight',()=>{console.log('reight');});
+    
+}
