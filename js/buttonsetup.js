3bb19400a5fb87e2cfe4caa259bb9f190b54daec
Siegert-S <149582140+Siegert-S@users.noreply.github.com>
2024-07-24 00:31:43 +0200

    button added

--- js/buttonsetup.js
+++ js/buttonsetup.js
@@ -3,15 +3,20 @@ let buttonHeight=25;
 let spaceBetweenVertical=10;
 
 function initButton() {
-    MenuButton.produce(canvasCenter(), 10, buttonWidth, buttonHeight, 'main', 'Play', () => { console.log('Play is pressed'); });
-    MenuButton.produce(canvasCenter(), 55, buttonWidth, buttonHeight, 'main', 'Settings', () => { console.log('Settings is pressed'); });
-    MenuButton.produce(canvasCenter(), 100, buttonWidth, buttonHeight, 'main', 'Controlls', () => { console.log('Controlls is pressed'); });
+    MenuButton.produce(canvasCenter(), stackButtons(0,3), buttonWidth, buttonHeight, 'main', 'Play',        () => { console.log('Play is pressed'); });
+    MenuButton.produce(canvasCenter(), stackButtons(1,3), buttonWidth, buttonHeight, 'main', 'Settings',    () => { console.log('Settings is pressed'); });
+    MenuButton.produce(canvasCenter(), stackButtons(2,3), buttonWidth, buttonHeight, 'main', 'Controlls',   () => { console.log('Controlls is pressed'); });
 
     MenuButton.produce(10, 430, 40, 40, 'controlls', 'left', () => { console.log('left'); });
     MenuButton.produce(60, 430, 40, 40, 'controlls', 'reight', () => { console.log('reight'); });
 
-    MenuButton.produce(canvasCenter(),10, buttonWidth,buttonHeight,'settings','Volume,'()=>{console.log('Volume on/off');});
-    MenuButton.produce(canvasCenter(),10, buttonWidth, buttonHeight,'settings','SFX',()=>{console.log('SFX sound on/off');});
+    MenuButton.produce(canvasCenter(),stackButtons(0,3), buttonWidth,buttonHeight,'settings','Volume,'()=>{console.log('Volume on/off');});
+    MenuButton.produce(canvasCenter(),stackButtons(1,3), buttonWidth, buttonHeight,'settings','SFX',()=>{console.log('SFX sound on/off');});
+    MenuButton.produce(canvasCenter(),stackButtons(2,3), buttonWidth,buttonHeight,'settings', 'Back to Main Menu',()=>{console.log('mainmenu');});
+    
+    MenuButton.produce(canvasCenter(),stackButtons(0,3), buttonWidth,buttonHeight,'legal','legal notice');
+    MenuButton.produce(canvasCenter(),stackButtons(1,3), buttonWidth,buttonHeight,'legal','privat polecey');
+    // den back t mainmenu kopieren
 }
 
 function canvasCenter() {
