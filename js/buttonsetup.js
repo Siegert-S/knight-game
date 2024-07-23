ee1c5189fa4498d62a33fdfccfa34bdcd622f92b
Siegert-S <149582140+Siegert-S@users.noreply.github.com>
2024-07-24 00:13:34 +0200

    button position function

--- js/buttonsetup.js
+++ js/buttonsetup.js
@@ -1,20 +1,30 @@
-let buttonWidth = 100
-
+let buttonWidth = 100;
+let buttonHeight=25;
+let spaceBetweenVertical=10;
 
 function initButton() {
-    MenuButton.produce(canvasCenter(), 10, buttonWidth, 25, 'main', 'Play', () => { console.log('Play is pressed'); });
-    MenuButton.produce(canvasCenter(), 55, buttonWidth, 25, 'main', 'Settings', () => { console.log('Settings is pressed'); });
-    MenuButton.produce(canvasCenter(), 100, buttonWidth, 25, 'main', 'Controlls', () => { console.log('Controlls is pressed'); });
+    MenuButton.produce(canvasCenter(), 10, buttonWidth, buttonHeight, 'main', 'Play', () => { console.log('Play is pressed'); });
+    MenuButton.produce(canvasCenter(), 55, buttonWidth, buttonHeight, 'main', 'Settings', () => { console.log('Settings is pressed'); });
+    MenuButton.produce(canvasCenter(), 100, buttonWidth, buttonHeight, 'main', 'Controlls', () => { console.log('Controlls is pressed'); });
 
     MenuButton.produce(10, 430, 40, 40, 'controlls', 'left', () => { console.log('left'); });
     MenuButton.produce(60, 430, 40, 40, 'controlls', 'reight', () => { console.log('reight'); });
 
+    MenuButton.produce(canvasCenter(),10, buttonWidth,buttonHeight,'settings','Volume,'()=>{console.log('Volume on/off');});
+    MenuButton.produce(canvasCenter(),10, buttonWidth, buttonHeight,'settings','SFX',()=>{console.log('SFX sound on/off');});
 }
 
 function canvasCenter() {
     return (canvas.width / 2) - (buttonWidth / 2);
 }
 
-function test() {
-    console.log('test');
+function stackButtons(index, amount) {
+    let center = canvas.height/2;
+if (amount==1) {
+    return center- buttonHeight/2;
+} else {
+    let startStack = center - ( (buttonHeight * amount) + (spaceBetweenVertical * (amount-1)) );
+    let position = startStack + (index * (buttonHeight + spaceBetweenVertical));
+    return position;
+}
 }
