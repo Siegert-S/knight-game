6611dfed3bde1af319b7f397e640e967b68b4429
Siegert-S <149582140+Siegert-S@users.noreply.github.com>
2024-07-25 00:29:12 +0200

    add buttons

--- js/buttonsetup.js
+++ js/buttonsetup.js
@@ -1,6 +1,6 @@
-let buttonWidth = 150;
-let buttonHeight = 25;
-let spaceBetweenVertical = 10;
+let buttonWidth = 175;
+let buttonHeight = 50;
+let spaceBetweenVertical = 20;
 
 function initButton() {
     MenuButton.produce(canvasCenter(), stackButtons(0, 3), buttonWidth, buttonHeight, 'main', 'Play', () => { console.log('Play is pressed'); });
@@ -12,14 +12,24 @@ function initButton() {
 
     MenuButton.produce(10, 430, 40, 40, 'controlls', 'left', () => { console.log('left'); });
     MenuButton.produce(60, 430, 40, 40, 'controlls', 'reight', () => { console.log('reight'); });
+    MenuButton.produce(110,430,40,40,'controlls', 'jump',()=>{ console.log('jump');});
+    MenuButton.produce(160,430,40,40,'controlls', 'attack',()=>{console.log('attack');});
+    MenuButton.produce(210,430,40,40,'controlls', 'defend',()=>{console.log('defend');});
 
-    MenuButton.produce(canvasCenter(), stackButtons(0, 3), buttonWidth, buttonHeight, 'settings', 'Volume', () => { console.log('Volume on/off'); });
-    MenuButton.produce(canvasCenter(), stackButtons(1, 3), buttonWidth, buttonHeight, 'settings', 'SFX', () => { console.log('SFX sound on/off'); });
-    MenuButton.produce(canvasCenter(), stackButtons(2, 3), buttonWidth, buttonHeight, 'settings', 'Back to Main Menu', () => { console.log('mainmenu'); switchTo('main'); });
+
+    MenuButton.produce(canvasCenter(), stackButtons(0, 5), buttonWidth, buttonHeight, 'settings', 'Volume', () => { console.log('Volume on/off'); });
+    MenuButton.produce(canvasCenter(), stackButtons(1, 5), buttonWidth, buttonHeight, 'settings', 'up',()=>{console.log('volume UP');});
+    MenuButton.produce(canvasCenter(), stackButtons(1, 5), buttonWidth, buttonHeight, 'settings', 'down',()=>{console.log('volume DOWN');});
+    MenuButton.produce(canvasCenter(), stackButtons(2, 5), buttonWidth, buttonHeight, 'settings', 'SFX', () => { console.log('SFX sound on/off'); });
+    MenuButton.produce(canvasCenter(), stackButtons(3, 5), buttonWidht, buttonHeight, 'settings', 'up',()=>{console.log('SFX UP');});
+    MenuButton.produce(canvasCenter(), stackButtons(3, 5), buttonWidht, buttonHeight, 'settings', 'down',()=>{console.log('SFX DOWN');});
+    MenuButton.produce(canvasCenter(), stackButtons(4, 5), buttonWidth, buttonHeight, 'settings', 'Back to Main Menu', () => { console.log('mainmenu'); switchTo('main'); });
 
     MenuButton.produce(canvasCenter(), stackButtons(0, 3), buttonWidth, buttonHeight, 'legal', 'legal notice');
     MenuButton.produce(canvasCenter(), stackButtons(1, 3), buttonWidth, buttonHeight, 'legal', 'privat polecey');
     // den back t mainmenu kopieren
+
+    MenuButton.produce(canvasCenter(), stackButtons(0,5), buttonWidth, buttonHeight, 'skill', 'strenght',()=>{console.log('increse power');});
 }
 
 function canvasCenter() {
